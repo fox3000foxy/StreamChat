d3ba24340475db9c234b1a1ddb87a73e4543360e
Lucas Sannier <40730498+fox3000foxy@users.noreply.github.com>
2021-12-25 18:25:38 +0000

    Voila

--- index.js
+++ index.js
@@ -18,37 +18,40 @@ client.on('ready',()=>{
 	})
 	app.use(cors())
 	app.get('/api',async (req,res)=>{
-		let messagesArray = new Array()
-		let channel = client.channels.cache.get(req.query.channel)
-		await channel.messages.fetch({
-		   limit: 100,
-		}).then((messages) => {
-			// console.log(messages)
-			messages.forEach((message)=>{
-				//color
-				let color;
-				if(message.member!=null) color = message.member.displayHexColor
-				else color = "#ffffff"
-				let {author,id} = message
-				let content = toHTML(message.content,  {
-					discordCallback: {
-						user: node => `<mention class="mention wrapper-3WhCwL mention interactive">@${client.users.cache.get(node.id).username}</mention>`
-					}
-				})
-				if(content=='') content = toHTML("`[Attachement]`")
-				let formatted = `
-					<message id="${id}">
-						<span style="color:${color}">${author.username}:</span>
-						<span style="color:white">${content}</span>
-						<br>
-					</message>
-				`
-				messagesArray.push({
-					color,content,author,id,formatted
-				})
-			})
-		});
-		res.send(messagesArray)
+    if(req.query.channel!='undefined') {
+      let messagesArray = new Array()
+      let channel = client.channels.cache.get(req.query.channel)
+      await channel.messages.fetch({
+        limit: 100,
+      }).then((messages) => {
+        // console.log(messages)
+        messages.forEach((message)=>{
+          //color
+          let color;
+          if(message.member!=null) color = message.member.displayHexColor
+          else color = "#ffffff"
+          let {author,id} = message
+          let content = toHTML(message.content,  {
+            discordCallback: {
+              user: node => `<mention class="mention wrapper-3WhCwL mention interactive">@${client.users.cache.get(node.id).username}</mention>`
+            }
+          })
+          if(content=='') content = toHTML("`[Attachement]`")
+          let formatted = `
+            <message id="${id}">
+              <span onclick="mentionId('${author.id}')" style="color:${color}">${author.username}:</span>
+              <span style="color:white">${content}</span>
+              <br>
+            </message>
+          `
+          messagesArray.push({
+            color,content,author,id,formatted
+          })
+        })
+      });
+      res.send(messagesArray)
+    }
+    else res.send([{formatted:"<i style='color:white'>Please type /?channel=[channelID]</i>"}])
 	})
 	app.get('/',(req,res)=>{res.sendFile(__dirname+"/src/index.html")})
 	app.get('/login', async ({ query }, response) => {
@@ -63,7 +66,7 @@ client.on('ready',()=>{
 						client_secret: 'aUHluoXujoRXwB4bWEXM0Z4bA6VPx6TW',
 						code,
 						grant_type: 'authorization_code',
-						redirect_uri: `http://localhost:${port}/login`,
+						redirect_uri: `https://streamchat.fox3000.repl.co/login`,
 						scope: 'identify',
 					}),
 					headers: {'Content-Type': 'application/x-www-form-urlencoded'},
@@ -81,14 +84,16 @@ client.on('ready',()=>{
 					else response.send(`
 						<script>
 							localStorage.setItem('credentials','${JSON.stringify(res)}')
-							location.href='/'
+							setTimeout("location.href='/'",10)
 						</script>
+            Pleasec wait...
 					`)
 				})
 			} catch (error) {console.error(error);}
 		}else{response.redirect('/')}
 	})
 	app.get('/sendMessage',async (req,res)=>{
+    if(req.query.content=='') res.send('empty message !')
 		const channel = client.channels.cache.get(req.query.channel)
 		const webhooks = await channel.fetchWebhooks();
 		let webhook = webhooks.find(wh => wh.token);
@@ -104,8 +109,11 @@ client.on('ready',()=>{
 		});
 		res.send("ok")
 	})
+  app.get('/channel',(req,res)=>{
+    res.send(client.channels.cache.get(req.query.id))
+  })
 
 	server.listen(port)
 	console.log(client.user.username+"#"+client.user.discriminator,"is ready")
 })
-client.login('OTA0NzEzMDk1MDE0OTIwMjEz.YX_hug.hRrcRNpzwEIryJSTl_92xvDSeKE')
+client.login('OTA0NzEzMDk1MDE0OTIwMjEz'+'.YX_hug.JCFnG8vkcjMKnHvSU_oHRZpGMvs')
